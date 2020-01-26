0682e7921bb3efa9139fb8a96f46693258a4e93c
Joel Feven <2306734F@student.gla.ac.uk>
2020-01-26 15:00:46 +0000

    fixed errors after running chapter 4 tests

--- rango/views.py
+++ rango/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 def index(request):
     # Construct a dictionary to pass to the template engine as its context.
     # Not they key boldmessage matches to {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie. candy, cupcake!'}
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     
     #return a rendered reposnse to send to the client
     #we make use of the shortcut function to make our lives easier
@@ -13,5 +13,5 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
     
 def about(request):
-    context_dict = {'boldmessage': 'This tutorial as been put together by Joel'}
+    context_dict = {'boldmessage': 'This tutorial has been put together by Joel'}
     return render(request, 'rango/about.html', context=context_dict)
